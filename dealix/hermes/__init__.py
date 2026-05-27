261cd6f4634a70d27f2f1b6d1d1c8d40e3451532
VoXc2 <Sami.assiri11@gmail.com>
2026-05-27 14:33:00 +0300

    hermes: add package init

---
+++ dealix/hermes/__init__.py
@@ -0,0 +1,9 @@
+"""Hermes Agent OS package.
+
+Hermes is a governed, approval-first agent operating layer for Dealix.
+It is designed to produce internal reports and action queues by default.
+"""
+
+__all__ = ["__version__"]
+
+__version__ = "0.1.0"
