8d3e561a4d98ab058b5f09ba771d9acbc4151820
VoXc2 <Sami.assiri11@gmail.com>
2026-06-26 06:43:13 +0300

    Add sales intelligence package

---
+++ app/sales_intelligence/__init__.py
@@ -0,0 +1,5 @@
+"""Dealix Sales Intelligence OS.
+
+This package prepares reviewable sales strategy, pain diagnosis, and negotiation guidance.
+It does not send messages or represent a company without explicit authorization.
+"""
