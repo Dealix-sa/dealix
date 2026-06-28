d8f8edc83ef7c9db186ba0e46a0d217e8befed40
VoXc2 <Sami.assiri11@gmail.com>
2026-06-29 00:05:20 +0300

    Add leadership growth command core

---
+++ app/leadership/growth_command_os.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+
+def run_executive_growth_command_day():
+    return {
+        'summary': {
+            'ceo_decisions': 1,
+            'growth_actions': 1,
+            'sales_actions': 1,
+            'partnership_actions': 1,
+            'marketing_actions': 1,
+            'live_sends': 0,
+            'live_commitments': 0,
+            'approval_required': 5,
+        },
+        'decision_cards': [],
+    }
