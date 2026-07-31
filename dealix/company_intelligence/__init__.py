ea8612461531a14548da121ec0c60db23c89abd3
VoXc2 <Sami.assiri11@gmail.com>
2026-07-31 13:03:28 +0300

    feat(company-intelligence): export execution spine contracts

--- dealix/company_intelligence/__init__.py
+++ dealix/company_intelligence/__init__.py
@@ -6,11 +6,29 @@ from dealix.company_intelligence.company_brain import (
     build_internal_company_brain,
 )
 from dealix.company_intelligence.contracts import CompanyBrainSource, ProvenanceRef
+from dealix.company_intelligence.execution_contracts import (
+    CanonicalApproval,
+    CanonicalDraft,
+    CanonicalOpportunity,
+    DraftChannel,
+    LawfulContactBasis,
+    build_draft,
+    normalize_approval,
+    normalize_opportunity,
+)
 
 __all__ = [
+    "CanonicalApproval",
     "CanonicalCompanyBrain",
+    "CanonicalDraft",
+    "CanonicalOpportunity",
     "CompanyBrainSource",
+    "DraftChannel",
+    "LawfulContactBasis",
     "ProvenanceRef",
     "build_customer_company_brain",
+    "build_draft",
     "build_internal_company_brain",
+    "normalize_approval",
+    "normalize_opportunity",
 ]
